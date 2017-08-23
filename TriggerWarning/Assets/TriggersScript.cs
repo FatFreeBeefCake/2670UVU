062a301335737767b5b5787d87d01e530de2e7b4
Unknown <stheadman1@hotmail.com>
2017-08-23 13:55:44 -0600

    Spencer Trigger lesson

---
+++ TriggerWarning/Assets/TriggersScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggersScript : MonoBehaviour {
+
+    public int Number = 10;
+    public GameObject Cube;
+    // Use this for initialization
+	void OnTriggerEnter () {
+        print(Number + 12);
+
+	}
+	
+	// Update is called once per frame
+	void OnTriggerExit () {
+        print(Number + 11);
+	}
+}
