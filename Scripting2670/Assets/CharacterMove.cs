fe461eaf7d5d249f9a60d4cd163162e394c1b0b1
Unknown <stheadman1@hotmail.com>
2017-08-28 13:58:12 -0600

    Class 8/28

---
+++ Scripting2670/Assets/CharacterMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+    CharacterController cc;
+
+	// Use this for initialization
+	void Start () {
+        cc = GetComponent<CharacterController>();
+        TriggersScript.KeyAction = Move;
+	}
+	
+	// Update is called once per frame
+	void Move (float _movement) {
+        print(_movement);
+	}
+}
---
+++ Scripting2670/Assets/TriggersScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TriggersScript : MonoBehaviour {
+
+    public static Action <float> KeyAction;
+	
+	// Update is called once per frame
+	void Update () {
+        if (KeyAction != null)
+        {
+         
+            KeyAction(Input.GetAxis("Horizontal"));
+            
+        }
+	}
+}
