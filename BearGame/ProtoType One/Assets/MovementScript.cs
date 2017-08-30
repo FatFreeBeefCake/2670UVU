b357a9bb70e7dd5fd28aefb2b0599e25a5f817ab
Unknown <stheadman1@hotmail.com>
2017-08-30 15:09:01 -0600

    BearGame Continued

---
+++ BearGame/ProtoType One/Assets/MovementScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+    public int movementspeed = 25;
+    public int jumpspeed = 100;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * movementspeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * movementspeed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.up * jumpspeed * Time.deltaTime);
+        }
+    }
+}
+
