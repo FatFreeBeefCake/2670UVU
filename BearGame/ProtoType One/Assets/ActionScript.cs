ddedfbc57eb54ee7141001691e8ed33537c0c108
Unknown <stheadman1@hotmail.com>
2017-08-31 11:43:05 -0600

    Prototype continues Development

---
+++ BearGame/ProtoType One/Assets/ActionScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class ActionScript : MonoBehaviour {
+
+    public static Action<float> MoveAction;
+    public static Action<float> JumpAction;
+
+	// Update is called once per frame
+	void Update () {
+        if (MoveAction != null)
+        {
+            MoveAction(Input.GetAxis("Horizontal"));
+        }
+        if (JumpAction != null)
+        {
+            JumpAction(Input.GetAxis("Vertical"));
+        }
+	}
+}
---
+++ BearGame/ProtoType One/Assets/PlayerMoveScript.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour {
+
+    CharacterController cc;
+
+    Vector3 tempMove;
+
+    public float speed = 3;
+    public float gravity = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        ActionScript.MoveAction += Move;
+        ActionScript.JumpAction += Jump;
+    }
+
+    private void Jump(float _movement)
+    {
+        tempMove.y = _movement * speed * Time.deltaTime;
+        cc.Move(tempMove);
+    }
+
+    // Update is called once per frame
+    void Move(float _movement)
+    {
+        tempMove.x = _movement * speed * Time.deltaTime;
+        cc.Move(tempMove);
+    }
+      
+}
