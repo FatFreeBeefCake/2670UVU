99e69a39f39e58136c5be74122c320af778fb72e
Unknown <stheadman1@hotmail.com>
2017-09-26 12:24:10 -0600

    A bunch of New Stuff

---
+++ BearGame/ProtoType One/Assets/NonMoveEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NonMoveEnemy : MonoBehaviour {
+
+    public PlayerMoveScript Character;
+    public Image healthbar;
+    public Transform Spawn;
+    public GameObject Player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Character = other.gameObject.GetComponent<PlayerMoveScript>();
+        other.gameObject.GetComponent<PlayerMoveScript>().Hit(25);
+
+        if (Character.Health < 1)
+        {
+            print("you Died");
+            Player.transform.position = Spawn.position;
+            Character.Health = 100;
+        }
+
+
+    }
+    public void Update()
+    {
+        transform.position = new Vector3(Mathf.PingPong(Time.time, 3) + 121.5f, transform.position.y, transform.position.z);
+    }
+}
