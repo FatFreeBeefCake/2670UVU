351de732456128186ecb5d83543d7f8e12579144
Unknown <stheadman1@hotmail.com>
2017-10-04 15:17:54 -0600

    Added, button/door

---
+++ BearGame/ProtoType One/Assets/DoorButtonscript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DoorButtonscript : MonoBehaviour {
+
+    public static Action OpenDoor;
+
+	void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            OpenDoor();
+        }
+    }
+}
---
+++ BearGame/ProtoType One/Assets/MoveObjectScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjectScript : MonoBehaviour {
+
+    Vector3 Box;
+
+	// Use this for initialization
+	void Start () {
+
+        DoorButtonscript.OpenDoor += Open;
+		
+	}
+
+    private void Open()
+    {
+        StartCoroutine(MoveBox());
+    }
+
+    IEnumerator MoveBox()
+    {
+        while (transform.position.y > -1)
+        {
+            Box.y = -3f * Time.deltaTime;
+            transform.Translate(Box);
+            yield return null;
+        }
+       
+    }
+}
