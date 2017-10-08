53d3faefb4cfda17c46e81f8a70db892a36e5f09
Unknown <stheadman1@hotmail.com>
2017-10-08 14:28:54 -0600

    Added FIrst part of temple
    Unity wont update its annoying

--- BearGame/ProtoType One/Assets/Scripts/MovingLRScript.cs
+++ BearGame/ProtoType One/Assets/Scripts/MovingLRScript.cs
@@ -7,7 +7,7 @@ public class MovingLRScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        transform.position = new Vector3(Mathf.PingPong(Time.time, 2) - 5, transform.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.PingPong(Time.time, 2), transform.position.y, transform.position.z);
 
     }
 }
--- BearGame/ProtoType One/Assets/Scripts/MovingPScriptNew.cs
+++ BearGame/ProtoType One/Assets/Scripts/MovingPScriptNew.cs
@@ -6,6 +6,6 @@ public class MovingPScriptNew : MonoBehaviour {
 
 
 	void Update () {
-        transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time, 3) + 3, transform.position.z);
+        transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time, 3) + 7, transform.position.z);
     }
 }
