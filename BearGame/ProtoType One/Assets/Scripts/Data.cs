fcc2e10901fdff61065a80cc110e772f0387e23d
Unknown <stheadman1@hotmail.com>
2017-10-13 14:09:37 -0600

    Coins YAY

---
+++ BearGame/ProtoType One/Assets/Scripts/Data.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+ public class Data
+{
+    public int totalScore;
+
+    public float health;
+
+    public string playerName;
+
+    public Vector3 checkPoint;
+
+    public int gold = 100;
+}
+
---
+++ BearGame/ProtoType One/Assets/UpdateGold.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateGold : MonoBehaviour {
+
+    public Text goldUI;
+
+    private void Awake()
+    {
+
+        SetGameData.UpdateGold += GoldHandler;
+        goldUI.GetComponent<Text>();
+    }
+
+    private void GoldHandler(int _gold)
+    {
+        goldUI.text = "Gold: $" + _gold.ToString();
+    }
+}
