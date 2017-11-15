b19d5272ace457eedb88deeb363c83d03aafda09
Unknown <stheadman1@hotmail.com>
2017-11-15 14:31:18 -0700

    Animations SHOULD be done.

---
+++ BearGame/ProtoType One/Assets/BearAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearAnimation : MonoBehaviour {
+
+    public Animator Bearani;
+
+    public bool Bear;
+
+    void Start()
+    {
+        Bearani = GetComponent<Animator>();
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.tag == "Player")
+        {
+            Bearani.SetTrigger("BearAttack");
+        }
+    }
+
+}
